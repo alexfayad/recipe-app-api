e57bb894bbb96ae2c37273e1253282d91dfa4667
Alexander Fayad <alexanderfayad@Alexanders-MacBook-Pro.local>
2020-05-08 19:38:12 -0400

    added test models and ability to create users

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        #Test new user with email or something
+        email = 'test@email.com'
+        password = 'testpassword123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+        
+        self.assertEquals(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        #Test user with weird email
+        email = 'test@EMAIL.com'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password='12345'
+        )
+
+        self.assertEquals(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        #Test user with email that aint an email
+        with self.assertRaises(ValueError):
+            user = get_user_model().objects.create_user(
+                email = None,
+                password='12345'
+            )
+
+    def test_create_new_superuser(self):
+        #Create a superuser
+        user = get_user_model().objects.create_superuser(
+            email='test@email.com',
+            password='12345'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
