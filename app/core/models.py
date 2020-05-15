9b58ce82278aca7977fbcd0cf1f49057dff50a99
Alexander Fayad <alexanderfayad@Alexanders-MacBook-Pro.local>
2020-05-15 02:01:35 -0400

    added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -46,3 +46,30 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Ingredient(models.Model):
+    # Ingredient to be use for a recipe
+    name = models.CharField(max_length=225)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    # Recipe object
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=225)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
